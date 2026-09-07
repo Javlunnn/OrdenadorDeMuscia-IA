00a92418e30d5b417ad6c25ff69543fc04f6231b
Javlunnn <114362139+Javlunnn@users.noreply.github.com>
2026-09-07 10:51:34 -0600

    Add files via upload

---
+++ Datocurioso.py
@@ -0,0 +1,12 @@
+import random
+
+datos_curiosos = [
+    "Los pulpos tienen tres corazones.",
+    "La miel nunca se echa a perder.",
+    "El Everest crece unos 4 mm cada año.",
+    "Un rayo es más caliente que la superficie del Sol.",
+    "Las abejas pueden reconocer rostros humanos."
+]
+
+print("¡Dato curioso del día!")
+print(random.choice(datos_curiosos))
